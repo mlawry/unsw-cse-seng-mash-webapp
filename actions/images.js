875b29b5ff31a1d3796e59b2d15434f6f0f56498
Lawrence Yao <lawrence.yao@unswalumni.com>
2014-08-02 14:06:44 +1000

    .

--- actions/images.js
+++ actions/images.js
@@ -1,1 +1,42 @@
-adfasfd
+var fs = require('fs');
+
+// Within this action function, you can write directly to the response parameter.
+// You must call response.end() when done to let HTTP server know the response is completed.
+function action(response, requestSearch, requestHash) {
+	var fileName;
+	switch (requestSearch) {
+	case "?1":
+		fileName = "1.png";
+		break;
+	case "?2":
+		fileName = "2.png";
+		break;
+	case "?3":
+		fileName = "3.png";
+		break;
+	default:
+		response.writeHead(404);
+		response.write("HTTP 404 File Not Found\n");
+		response.end();
+		return;
+	}
+	var filePath = "./data/" + fileName;
+	
+	fs.readFile(filePath, function(err, buffer) {
+		if (err) {
+			// Failed to read the image file data.
+			response.writeHead(500);
+			console.log("Error reading file " + filePath + ": " + JSON.stringify(err));
+		} else {
+			// Return the image data.
+			response.writeHead(200, {
+				'Content-Type': 'image/png'
+			});
+			response.write(buffer);
+			console.log("Successfully sent the contents of file: " + filePath);
+		}
+		response.end();
+	});
+}
+
+module.exports.action = action;
--- server.js
+++ server.js
@@ -14,14 +14,14 @@ function start(routeFunc) {
 		
 		if (typeof actionFunc !== "function") {
 			// Router did not manage to find an appropriate action, so we return 404.
-			response.writeHead(404);
+			response.writeHead(404, {"Content-Type": "text/plain"});
+			response.write("HTTP 404 Not Found\n");
+			response.end();
 			console.log("Server response is 404 Not Found.");
 		} else {
 			// The action will write appropriate content to response based on search and hash. 
 			actionFunc(response, parsed.search, parsed.hash);
 		}
-
-		response.end();
 	}
 
 	http.createServer(onRequest).listen(8080);
